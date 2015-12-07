bca0ff2376f18e6c12515fd9f432c3bba3042307
Theodore Abshire <TheodoreAbshire@Gmail.com>
2015-12-07 13:00:44 -0800

    Initial Commit

---
+++ StackApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackApp
+//
+//  Created by Theodore Abshire on 12/7/15.
+//  Copyright © 2015 TheodoreAbshire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StackApp
+//
+//  Created by Theodore Abshire on 12/7/15.
+//  Copyright © 2015 TheodoreAbshire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
